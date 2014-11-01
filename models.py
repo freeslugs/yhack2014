3477fe1998bf9228a9c740c8e9e8bebc62a5d88b
Olalekan Afuye <lekez2005@yahoo.ca>
2014-11-01 15:34:46 -0400

    create models

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
-__author__ = 'lekez2005'
+from mongoengine import StringField, DecimalField, ReferenceField, ListField
+
+from app import db
+
+class Tag(db.Document):
+	tag_id = StringField(required=True)
+	prob = DecimalField(required=True, precision=10)
+
+
+class Movie(db.Document):
+	name = StringField(required=True)
+	tags = ListField(ReferenceField(Tag))
