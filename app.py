82108de3a0a9074fa1031770b552ed5ce22af379
Artur Renault <artur.renault@gmail.com>
2014-10-31 22:35:08 -0400

    Static folder working

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run()
