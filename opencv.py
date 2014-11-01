a52b6773e080cefe9d2f2dd9afa5b173d1fbc086
freeslugs <freeslugs@gmail.com>
2014-11-01 14:43:34 -0400

    update opencv python

--- opencv.py
+++ opencv.py
@@ -1,9 +1,7 @@
 import numpy as np
 import cv2
 from cv2.cv import CV_CAP_PROP_FPS
-
 import clarifai
-
 
 def extract_tags(filename, interval=2):
 	""" Return the tags in the movie in filename
@@ -12,7 +10,7 @@ def extract_tags(filename, interval=2):
 	cap = cv2.VideoCapture(filename)
 	FPS = cap.get(CV_CAP_PROP_FPS)
 	frame_interval = int(round(interval * FPS))
-	frames = []
+	imgs = []
 
 	i = 0
 	while (cap.isOpened()):
@@ -22,23 +20,17 @@ def extract_tags(filename, interval=2):
 		if i % frame_interval == 0:
 			height, width = frame.shape[:2]
 			if width > 1024:
-				# print width
 				scale = 1024 / float(width)
-				# print scale
 				img = cv2.resize(frame, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-				# print img.shape[:2]
 			else:
 				img = frame
 			img_name = "temp.png"
 			cv2.imwrite(img_name, img)
 			tags = clarifai.get_tags_from_file(img_name)
-			#tags['frame'] = i
 
-			frames.append(tags)
+			imgs.append(tags)
 
 		i += 1
 
 	cap.release()
-	return frames
-
-print extract_tags('test.mov')
+	return imgs
