e7a5145ff3faa73b972bcde5975dbd7cac04506b
Olalekan Afuye <lekez2005@yahoo.ca>
2014-11-02 05:26:05 -0500

    Tag fetch api

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 import operator
 import models
 
+
 def remove_outliers(imgs, tolerance=10):
 	tags = {}
 	for img in imgs:
@@ -12,7 +13,7 @@ def remove_outliers(imgs, tolerance=10):
 				tags[key] = img[key]
 
 	top = dict(sorted(tags.iteritems(), key=operator.itemgetter(1), reverse=True)[:tolerance])
-	
+
 	new_imgs = []
 
 	for img in imgs:
@@ -23,22 +24,20 @@ def remove_outliers(imgs, tolerance=10):
 		new_imgs.append(new_img)
 
 	return new_imgs
- 
- 
+
+
 def get_tag(tag):
-     movies = models.Movie.objects()
-     mvs = {}
-     probs = []
-     for movie in movies:
-         sum_all_tags = sum( map(lambda x: x.values()[0], movie.imgs) )
-         sum_tag = sum( map(lambda x: x.values()[0], filter(lambda y: y.keys()[0]==tag, movie.imgs)) )
-         probability = (sum_tag+0.0)/sum_all_tags
-         print sum_tag
-         print sum_all_tags
-         mvs[movie.name] = probability
-         probs.append(probability)
-     
-     #return sorted(mvs.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
-     return probs
-     
+	movies = models.Movie.objects()
+	mvs = {}
+	for movie in movies:
+		sum_all_tags = sum([sum(i) for i in (map(lambda x: x.values(), movie.imgs))])
+		sum_tag = 0
+		for frame in movie.imgs:
+			if tag in frame:
+				sum_tag += frame.values()[0]
+		probability = (sum_tag) / sum_all_tags
+		mvs[movie.name] = probability
+
+	return sorted(mvs.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
+
 
