f6243d0b2931ac78bfb47192c78792b054e30b4c
Olalekan Afuye <lekez2005@yahoo.ca>
2014-11-02 02:35:18 -0500

    get tags for movies

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import operator
+import models
 
 def remove_outliers(imgs, tolerance=10):
 	tags = {}
@@ -22,4 +23,22 @@ def remove_outliers(imgs, tolerance=10):
 		new_imgs.append(new_img)
 
 	return new_imgs
+ 
+ 
+def get_tag(tag):
+     movies = models.Movie.objects()
+     mvs = {}
+     probs = []
+     for movie in movies:
+         sum_all_tags = sum( map(lambda x: x.values()[0], movie.imgs) )
+         sum_tag = sum( map(lambda x: x.values()[0], filter(lambda y: y.keys()[0]==tag, movie.imgs)) )
+         probability = (sum_tag+0.0)/sum_all_tags
+         print sum_tag
+         print sum_all_tags
+         mvs[movie.name] = probability
+         probs.append(probability)
+     
+     #return sorted(mvs.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
+     return probs
+     
 
