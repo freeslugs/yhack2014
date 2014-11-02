e9328a50a3b50a25f83a4fd9389070ee721bc2a0
freeslugs <freeslugs@gmail.com>
2014-11-02 01:58:53 -0500

    upload form working

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, send_from_directory
 from flask.ext import restful
 from flask.ext.mongoengine import MongoEngine
 from mongoengine import *
 from flask.ext.cors import CORS, cross_origin
 import models #import Movie, Image
 from werkzeug.exceptions import BadRequestKeyError
-
+import os
+from werkzeug import secure_filename
+import requests
+import json
 app = Flask(__name__)
 cors = CORS(app)
+
+UPLOAD_FOLDER = './movies'
+ALLOWED_EXTENSIONS = set(['mp4', 'avi', 'mov'])
+
+# app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.debug = True
 app.config["MONGODB_SETTINGS"] = {'DB': "yhacks"}
@@ -20,14 +29,29 @@ api = restful.Api(app)
 
 db = MongoEngine(app)
 
-#def get_imgs_from_list(l):
-#	for img in l:
-#		img = Image(tag=img.tag,)
 
-@app.route("/")
-def index():
-	return app.send_static_file("index.html")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			url = "http://localhost:5000/api/add-movie"
+			data = { 'name': request.form['name'], 'filename': filename, 'interval': 10, 'upload': True }
+			headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+			r = requests.post(url, data=json.dumps(data), headers=headers)
+			return "success"
+    return app.send_static_file("index.html")
+
+@app.route('/movies/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 class GetMovie(restful.Resource):
 	def get(self):
